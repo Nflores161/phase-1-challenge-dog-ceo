1524b23d7856295ddbf139d3f2d0cbc3b6aea801
Nick Flores-Tober <nflores161@gmail.com>
2021-04-12 15:02:34 -0400

    Work so far

--- src/index.js
+++ src/index.js
@@ -1,1 +1,29 @@
 console.log('%c HI', 'color: firebrick')
+
+let dogImg = document.querySelector("div#dog-image-container");
+let dogUl = document.createElement('ul');
+
+
+  fetch("https://dog.ceo/api/breeds/image/random/4")
+  .then(resp => resp.json())
+  .then((respPojo) => {
+    // console.log(respPojo)
+    respPojo.message.forEach(function(element) {
+      let blankLi = document.createElement('li');
+      let blankImg = document.createElement('img');
+
+      blankImg.src = element;
+      // console.log(blankImg.src);
+
+      blankLi.append(blankImg);
+      dogImg.append(blankLi)
+    })
+  })
+
+  fetch("https://dog.ceo/api/breeds/list/all")
+  .then(resp => resp.json())
+  .then((response) => {
+    response.message.forEach(function(element){
+      console.log(element);
+    })
+  });
